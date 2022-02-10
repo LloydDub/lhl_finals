02087f2489fcc845b55e72f5776fb4a874cf566f
LloydDub <lloydrww@gmail.com>
2022-02-09 19:18:01 -0700

    makes metadata in json nowl

--- nft_pixel_maker/index.js
+++ nft_pixel_maker/index.js
@@ -1,19 +1,54 @@
 const fs = require("fs");
+const myArgs = process.argv.slice(2);
 const { createCanvas, loadImage } = require("canvas");
-const canvas = createCanvas(1000, 1000);
-const ctx = canvas.getContext("2d");
 const { layers, width, height } = require("./input/config");
-const edition = 5;
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext("2d");
+// sets the amount to make  via terminal
+const edition = myArgs.length > 0 ? Number(myArgs[0]) : 1;
+let metadata = [];
+let attributes = [];
+let hash = [];
+let decodedHash = [];
 
 const saveLayer = (_canvas, _edition) => {
   fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
-  console.log("Image Made");
+};
+
+//pushes meta data for nft to JSON
+const addMetadata = (_edition) => {
+  let dateTime = Date.now();
+  let tempMetadata = {
+    hash: hash.join(""),
+    decodedHash: "",
+    edition: _edition,
+    dateTime: dateTime,
+    attributes: attributes,
+  };
+  metadata.push(tempMetadata);
+  attributes = [];
+  hash = [];
+  decodedHash = [];
+};
+
+const addAttributes = (_element, _layer) => {
+  let tempAttr = {
+    id: _element.id,
+    layer: _layer.name,
+    name: _element.name,
+    rarity: _element.rarity,
+  };
+  attributes.push(tempAttr);
+  hash.push(_layer.id);
+  hash.push(_element.id);
+  decodedHash.push({ [_layer.id]: _element.id });
 };
 
 // applies layers randomly to create our editions.
 const drawLayer = async (_layer, _edition) => {
   let element =
     _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
+  addAttributes(element, _layer);
 
   const image = await loadImage(`${_layer.location}${element.fileName}`);
   ctx.drawImage(
@@ -23,9 +58,7 @@ const drawLayer = async (_layer, _edition) => {
     _layer.size.width,
     _layer.size.height
   );
-  console.log(
-    `I created the ${_layer.name} layer, and choose element ${element.name}`
-  );
+
   saveLayer(canvas, _edition);
 };
 
@@ -34,5 +67,12 @@ for (let i = 1; i <= edition; i++) {
   layers.forEach((layer) => {
     drawLayer(layer, i);
   });
+  addMetadata(i);
   console.log("Creating edition " + i);
 }
+//
+
+fs.readFile("./output/_metadata.json", (err, data) => {
+  if (err) throw err;
+  fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata));
+});
