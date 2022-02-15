e8378d34e2e297518fa1724b942bf158b317e737
LloydDub <lloydrww@gmail.com>
2022-02-15 14:53:06 -0700

    merge

--- nft_pixel_maker/index.js
+++ nft_pixel_maker/index.js
@@ -1,78 +1,190 @@
 const fs = require("fs");
-const myArgs = process.argv.slice(2);
 const { createCanvas, loadImage } = require("canvas");
-const { layers, width, height } = require("./input/config");
+const {
+  width, 
+  height, 
+  description, 
+  baseImageUri, 
+  startEditionFrom, 
+  endEditionAt, 
+  editionSize,
+  ramenWeights,
+  ramenBowls  } = require("./input/config");
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext("2d");
-// sets the amount to make  via terminal
-const edition = myArgs.length > 0 ? Number(myArgs[0]) : 1;
-let metadata = [];
-let attributes = [];
-let hash = [];
-let decodedHash = [];
 
-const saveLayer = (_canvas, _edition) => {
-  fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
+let metadataList = [];
+let attributesList = [];
+let dnaList = [];
+
+const saveImage = (_editionCount) => {
+  fs.writeFileSync(`./output/${_editionCount}.png`, canvas.toBuffer("image/png"));
 };
+
+const signImage = (_sig) => {
+  ctx.fillStyle = "#000000";
+  ctx.font = "bold italics 20pt Arial";
+  ctx.textBaseline = "top";
+  ctx.textAlign = "left";
+  ctx.fillText(_sig, 20, 20);
+};
+
+const genColour = () => {
+  let hue = Math.floor(Math.random() * 3600);
+  let pastel = `hsl(${hue}, 100%, 85%)`;
+  return pastel;
+}
+
+const drawBackground = () => {
+  ctx.fillStyle = genColour();
+  ctx.fillRect(0, 0, width, height);
+}
 
 //pushes meta data for nft to JSON
-const addMetadata = (_edition) => {
+const addMetadata = (_dna, _edition) => {
   let dateTime = Date.now();
   let tempMetadata = {
-    hash: hash.join(""),
-    decodedHash: "",
+    dna: _dna.join(""),
+    name: `#${_edition}`,
+    description: description,
+    image: `${baseImageUri}/${_edition}`,
     edition: _edition,
     dateTime: dateTime,
-    attributes: attributes,
+    attributes: attributesList
   };
-  metadata.push(tempMetadata);
-  attributes = [];
-  hash = [];
-  decodedHash = [];
+  metadataList.push(tempMetadata);
+  attributesList = [];
 };
 
-const addAttributes = (_element, _layer) => {
-  let tempAttr = {
-    id: _element.id,
-    layer: _layer.name,
-    name: _element.name,
-    rarity: _element.rarity,
-  };
-  attributes.push(tempAttr);
-  hash.push(_layer.id);
-  hash.push(_element.id);
-  decodedHash.push({ [_layer.id]: _element.id });
+const addAttributes = (_element) => {
+  let selectedElement = _element.layer.selectedElement;
+  attributesList.push({
+    name: selectedElement.name,
+    rarity: selectedElement.rarity
+  })
 };
 
 // applies layers randomly to create our editions.
-const drawLayer = async (_layer, _edition) => {
-  let element =
-    _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
-  addAttributes(element, _layer);
-
-  const image = await loadImage(`${_layer.location}${element.fileName}`);
-  ctx.drawImage(
-    image,
-    _layer.position.x,
-    _layer.position.y,
-    _layer.size.width,
-    _layer.size.height
-  );
-
-  saveLayer(canvas, _edition);
+const loadLayerImg = async (_layer) => {
+  return new Promise(async (resolve) => {
+    const image = await loadImage(
+      `${_layer.selectedElement.path}`
+      );
+    resolve({layer: _layer, loadedImage: image})
+  });
 };
 
-// we want to loop over editon create a piece and loo over the layers obect.
-for (let i = 1; i <= edition; i++) {
-  layers.forEach((layer) => {
-    drawLayer(layer, i);
-  });
-  addMetadata(i);
-  console.log("Creating edition " + i);
-}
-//
+const drawElement = (_element) => {
+  ctx.drawImage(
+    _element.loadedImage,
+    _element.layer.position.x,
+    _element.layer.position.y,
+    _element.layer.size.width,
+    _element.layer.size.height
+  );
+  addAttributes(_element);
+};
 
-fs.readFile("./output/_metadata.json", (err, data) => {
-  if (err) throw err;
-  fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata));
-});
+const constructLayerToDna = (_dna = [], _ramenBowls = [], _ramenBowl) => {
+  console.log(_dna)
+  let ramenBowl = _ramenBowl.toLowerCase();
+
+  let mappedDnaToLayers = _ramenBowls[ramenBowl].layers.map((layer, index) => {
+    
+    let selectedElement = layer.elements.find((e) => e.id == _dna[index]);
+    return {
+      position: layer.position,
+      size: layer.size,
+      selectedElement: selectedElement
+    };
+  });
+  return mappedDnaToLayers;
+};
+
+const getRamenBowl = (_randNum) => {
+  let ramenBowl = "";
+  ramenWeights.forEach(ramenWeight => {
+    if(_randNum >= ramenWeight.from && _randNum <= ramenWeight.to) {
+      ramenBowl = ramenWeight.value.toLowerCase();
+    }
+  })
+  return ramenBowl;
+};
+
+const isDnaUnique = (_DnaList = [], _dna = []) => {
+  let foundDna =  _DnaList.find((i) => i.join("") === _dna.join(""));
+  return foundDna == undefined ? true : false;
+};
+
+const createDna = (_ramenBowls, _ramenBowl) => {
+  let randNum = [];
+  let ramenBowl = _ramenBowl.toLowerCase();
+
+  _ramenBowls[ramenBowl].layers.forEach((layer) => {
+    let randElementNum = Math.floor(Math.random() * 100);
+    let num = 0;
+    layer.elements.forEach((element) => {
+      if (randElementNum >= 100 - element.weight) {
+        num = element.id;
+      }
+    })
+    randNum.push(num);
+  });
+
+  return randNum;
+};
+
+const writeMetadata = (_data) => {
+  fs.writeFileSync("./output/_metadata.json", _data);
+};
+
+const startCreating = async () => {
+  //clears metadata every time you run the file
+  writeMetadata("");
+  // we want to loop over editon create a piece and loo over the layers obect.
+  let editionCount = startEditionFrom;
+
+  while(editionCount <= endEditionAt) {
+    let randNum = Math.floor(Math.random() * editionSize)
+    let ramenBowl = getRamenBowl(randNum);
+    console.log("Random number to determine background (ramenWeights): ", randNum)
+    
+    let newDna = createDna(ramenBowls, ramenBowl)
+
+    console.log("Background chosen via ramenWeights: ", ramenBowl)
+    console.log(`New DNA: ${newDna}`);
+    // everything in if statement will run if dna is not found in dnalist
+    
+    if (isDnaUnique(dnaList, newDna)) {
+      
+      let results = constructLayerToDna(newDna, ramenBowls, ramenBowl);
+      let loadedElements = [];
+      // pushing each promise loadedElements PROMISE array
+      results.forEach(layer => {
+        loadedElements.push(loadLayerImg(layer));
+      })
+      
+      // takes in array of promises
+      await Promise.all(loadedElements).then(elementArray => {
+        drawBackground();
+        elementArray.forEach(element => {
+          drawElement(element);
+        });
+        signImage(`#${editionCount}/${editionSize}`);
+        saveImage(editionCount);
+        addMetadata(newDna, editionCount);
+        console.log(`New DNA is unique. Created edition ${editionCount} with DNA: ${newDna}`);
+      });
+        // Add new DNA (only if unique) to DNA List
+      dnaList.push(newDna)
+      // only increment counter if DNA is unique else log "DNA Exists" and don't increment
+      editionCount++;
+    } else {
+      console.log("DNA exists")
+    }
+  }
+  console.log("Final DNA List: ", dnaList);
+  writeMetadata(JSON.stringify(metadataList));
+};
+
+startCreating();
--- nft_pixel_maker/input/config.js
+++ nft_pixel_maker/input/config.js
@@ -3,6 +3,7 @@ const width = 1000;
 const height = 1000;
 const dir = __dirname;
 const description = "This is an NFT made by our generative art engine.";
+<<<<<<< HEAD
 
 const startEditionFrom = 1;
 const endEditionAt = 10;
@@ -695,3 +696,692 @@ module.exports = {
   ramenBowls,
   ramenWeights,
 };
+=======
+const baseImageUri = "https:///hashlips/nft/1";
+const startEditionFrom = 1;
+const endEditionAt = 10;
+const editionSize = 100;
+
+const ramenWeights = [
+  {
+    value: "Gold",
+    from: 1,
+    to: 2
+  },
+  {
+    value: "Brown",
+    from: 3,
+    to: 40
+  },
+  {
+    value: "White",
+    from: 41,
+    to: editionSize
+  },
+];
+
+//think about how your layers are built up!
+const ramenBowls = {
+  white: {
+    name: "White",
+    layers: [{
+      name: "Background",
+      elements: [
+      {
+        id: 0,
+        name: "White",
+        path: `${dir}/white/background_3/whiteBack-01.png`,
+        weight: 100
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Broth",
+      elements: [
+      {
+        id: 0,
+        name: "Miso",
+        path: `${dir}/white/broth_4/misoBroth-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Shio",
+        path: `${dir}/white/broth_4/shioBroth-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Shoyu",
+        path: `${dir}/white/broth_4/shoyuBroth-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Tonk",
+        path: `${dir}/white/broth_4/tonkBroth-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Noodle",
+      elements: [
+      {
+        id: 0,
+        name: "Ramen",
+        path: `${dir}/white/noodle_4/ramenNoodle-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Soba",
+        path: `${dir}/white/noodle_4/sobaNoodle-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Somen",
+        path: `${dir}/white/noodle_4/somenNoodle-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Udon",
+        path: `${dir}/white/noodle_4/udonNoodle-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Protein",
+      elements: [
+      {
+        id: 0,
+        name: "Tofu",
+        path: `${dir}/white/protein_4/tofu-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Beef",
+        path: `${dir}/white/protein_4/beef-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Pork",
+        path: `${dir}/white/protein_4/pork-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Shrimp",
+        path: `${dir}/white/protein_4/shrimp-01.png`,
+        weight: 5
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Onion",
+      elements: [
+      {
+        id: 0,
+        name: "Leek",
+        path: `${dir}/white/onion_3/leek.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Scallion",
+        path: `${dir}/white/onion_3/scallion-01.png`,
+        weight: 25
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Mushroom",
+      elements: [
+      {
+        id: 0,
+        name: "Enoki",
+        path: `${dir}/white/mushroom_2/enoki-01.png`,
+        weight: 60
+      },
+      {
+        id: 1,
+        name: "Shitake",
+        path: `${dir}/white/mushroom_2/shitake-01.png`,
+        weight: 40
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Veg",
+      elements: [
+      {
+        id: 0,
+        name: "Bok Choy",
+        path: `${dir}/white/veg_3/bokChoy-01.png`,
+        weight: 55
+      },
+      {
+        id: 1,
+        name: "Cilantro",
+        path: `${dir}/white/veg_3/cilantro-01.png`,
+        weight: 45
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Egg",
+      elements: [
+      {
+        id: 0,
+        name: "Boiled",
+        path: `${dir}/white/egg_2/boiledEgg.png`,
+        weight: 65
+      },
+      {
+        id: 1,
+        name: "Fried",
+        path: `${dir}/white/egg_2/friedEgg-01.png`,
+        weight: 35
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Garnish",
+      elements: [
+      {
+        id: 0,
+        name: "Horse Radish",
+        path: `${dir}/white/garnish_6/horseRadish-01.png`,
+        weight: 40
+      },
+      {
+        id: 1,
+        name: "White Sesame",
+        path: `${dir}/white/garnish_6/whiteSes-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Corn",
+        path: `${dir}/white/garnish_6/corn-01.png`,
+        weight: 20
+      },
+      {
+        id: 3,
+        name: "Black Sesame",
+        path: `${dir}/white/garnish_6/blackSesame-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+  ]},
+  brown: {
+    name: "Brown",
+    layers: [{
+      name: "Background",
+      elements: [
+      {
+        id: 0,
+        name: "Brown",
+        path: `${dir}/brown/background_3/brownBack.png`,
+        weight: 100
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Broth",
+      elements: [
+      {
+        id: 0,
+        name: "Miso",
+        path: `${dir}/brown/broth_4/misoBroth-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Shio",
+        path: `${dir}/brown/broth_4/shioBroth-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Shoyu",
+        path: `${dir}/brown/broth_4/shoyuBroth-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Tonk",
+        path: `${dir}/brown/broth_4/tonkBroth-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Noodle",
+      elements: [
+      {
+        id: 0,
+        name: "Ramen",
+        path: `${dir}/brown/noodle_4/ramenNoodle-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Soba",
+        path: `${dir}/brown/noodle_4/sobaNoodle-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Somen",
+        path: `${dir}/brown/noodle_4/somenNoodle-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Udon",
+        path: `${dir}/brown/noodle_4/udonNoodle-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Protein",
+      elements: [
+      {
+        id: 0,
+        name: "Tofu",
+        path: `${dir}/brown/protein_4/tofu-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Beef",
+        path: `${dir}/brown/protein_4/beef-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Pork",
+        path: `${dir}/brown/protein_4/pork-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Shrimp",
+        path: `${dir}/brown/protein_4/shrimp-01.png`,
+        weight: 5
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Onion",
+      elements: [
+      {
+        id: 0,
+        name: "Leek",
+        path: `${dir}/brown/onion_3/leek.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Scallion",
+        path: `${dir}/brown/onion_3/scallion-01.png`,
+        weight: 25
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Veg",
+      elements: [
+      {
+        id: 0,
+        name: "Bok Choy",
+        path: `${dir}/brown/veg_3/bokChoy-01.png`,
+        weight: 55
+      },
+      {
+        id: 1,
+        name: "Cilantro",
+        path: `${dir}/brown/veg_3/cilantro-01.png`,
+        weight: 45
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Mushroom",
+      elements: [
+      {
+        id: 0,
+        name: "Enoki",
+        path: `${dir}/brown/mushroom_2/enoki-01.png`,
+        weight: 60
+      },
+      {
+        id: 1,
+        name: "Shitake",
+        path: `${dir}/brown/mushroom_2/shitake-01.png`,
+        weight: 40
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Egg",
+      elements: [
+      {
+        id: 0,
+        name: "Boiled",
+        path: `${dir}/brown/egg_2/boiledEgg.png`,
+        weight: 65
+      },
+      {
+        id: 1,
+        name: "Fried",
+        path: `${dir}/brown/egg_2/friedEgg-01.png`,
+        weight: 35
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Garnish",
+      elements: [
+      {
+        id: 0,
+        name: "Horse Radish",
+        path: `${dir}/brown/garnish_6/horseRadish-01.png`,
+        weight: 40
+      },
+      {
+        id: 1,
+        name: "White Sesame",
+        path: `${dir}/brown/garnish_6/whiteSes-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Corn",
+        path: `${dir}/brown/garnish_6/corn-01.png`,
+        weight: 20
+      },
+      {
+        id: 3,
+        name: "Black Sesame",
+        path: `${dir}/brown/garnish_6/blackSesame-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    }
+    ]
+  },
+  gold: {
+    name: "Gold",
+    layers: [{
+      name: "Background",
+      elements: [
+      {
+        id: 0,
+        name: "Gold",
+        path: `${dir}/gold/background_3/goldBack-01.png`,
+        weight: 100
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Broth",
+      elements: [
+      {
+        id: 0,
+        name: "Miso",
+        path: `${dir}/gold/broth_4/misoBroth-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Shio",
+        path: `${dir}/gold/broth_4/shioBroth-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Shoyu",
+        path: `${dir}/gold/broth_4/shoyuBroth-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Tonk",
+        path: `${dir}/gold/broth_4/tonkBroth-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Noodle",
+      elements: [
+      {
+        id: 0,
+        name: "Ramen",
+        path: `${dir}/gold/noodle_4/ramenNoodle-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Soba",
+        path: `${dir}/gold/noodle_4/sobaNoodle-01.png`,
+        weight: 25
+      },
+      {
+        id: 2,
+        name: "Somen",
+        path: `${dir}/gold/noodle_4/somenNoodle-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Udon",
+        path: `${dir}/gold/noodle_4/udonNoodle-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Protein",
+      elements: [
+      {
+        id: 0,
+        name: "Tofu",
+        path: `${dir}/gold/protein_4/tofu-01.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Beef",
+        path: `${dir}/gold/protein_4/beef-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Pork",
+        path: `${dir}/gold/protein_4/pork-01.png`,
+        weight: 15
+      },
+      {
+        id: 3,
+        name: "Shrimp",
+        path: `${dir}/gold/protein_4/shrimp-01.png`,
+        weight: 5
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Veg",
+      elements: [
+      {
+        id: 0,
+        name: "Bok Choy",
+        path: `${dir}/gold/veg_3/bokChoy-01.png`,
+        weight: 55
+      },
+      {
+        id: 1,
+        name: "Cilantro",
+        path: `${dir}/gold/veg_3/cilantro-01.png`,
+        weight: 45
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Onion",
+      elements: [
+      {
+        id: 0,
+        name: "Leek",
+        path: `${dir}/gold/onion_3/leek.png`,
+        weight: 50
+      },
+      {
+        id: 1,
+        name: "Scallion",
+        path: `${dir}/gold/onion_3/scallion-01.png`,
+        weight: 25
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Mushroom",
+      elements: [
+      {
+        id: 0,
+        name: "Enoki",
+        path: `${dir}/gold/mushroom_2/enoki-01.png`,
+        weight: 60
+      },
+      {
+        id: 1,
+        name: "Shitake",
+        path: `${dir}/gold/mushroom_2/shitake-01.png`,
+        weight: 40
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Egg",
+      elements: [
+      {
+        id: 0,
+        name: "Boiled",
+        path: `${dir}/gold/egg_2/boiledEgg.png`,
+        weight: 65
+      },
+      {
+        id: 1,
+        name: "Fried",
+        path: `${dir}/gold/egg_2/friedEgg-01.png`,
+        weight: 35
+      }
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    },
+    {
+      name: "Garnish",
+      elements: [
+      {
+        id: 0,
+        name: "Horse Radish",
+        path: `${dir}/gold/garnish_6/horseRadish-01.png`,
+        weight: 40
+      },
+      {
+        id: 1,
+        name: "White Sesame",
+        path: `${dir}/gold/garnish_6/whiteSes-01.png`,
+        weight: 30
+      },
+      {
+        id: 2,
+        name: "Corn",
+        path: `${dir}/gold/garnish_6/corn-01.png`,
+        weight: 20
+      },
+      {
+        id: 3,
+        name: "Black Sesame",
+        path: `${dir}/gold/garnish_6/blackSesame-01.png`,
+        weight: 10
+      },
+      ],
+      position: { x: 0, y: 0 },
+      size: { width: width, height: height },
+    }
+    ]
+  },
+}
+
+module.exports = {
+  width, 
+  height, 
+  description, 
+  baseImageUri, 
+  startEditionFrom, 
+  endEditionAt, 
+  editionSize,
+  ramenBowls,
+  ramenWeights };
+>>>>>>> art_engine
