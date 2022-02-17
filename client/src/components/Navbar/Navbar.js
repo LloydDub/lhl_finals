98f8a94ee819f9b8599f55fb19697d658f195903
Dara A <daraakitoye1@gmail.com>
2022-02-17 03:57:04 -0700

    Starter code for front-end and navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+    <nav>
+      <h1 className="navbar-logo">Title</h1>
+    <div className="navbar-icon"></div>
+    </nav>
+    </>
+  );
+}
