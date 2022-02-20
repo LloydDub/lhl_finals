3bb3bccf87be13b8c768a55215b74be314731838
Dara A <daraakitoye1@gmail.com>
2022-02-20 11:59:48 -0700

    Sets up file stucture

--- client/client_2/src/App.jsx
+++ client/client_2/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <div className="App">
-     
+     <h1 className='text-3x1 font bold underline'>
+       Hello world!
+     </h1>
     </div>
   )
 }
